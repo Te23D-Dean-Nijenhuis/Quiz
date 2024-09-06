dbe280b4e204dd69616cc0216cff9ce959194694
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2024-09-06 10:25:12 +0200

    sdsfk

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int points = 0; 
+bool correct = false;
+
+Console.WriteLine("Hej och välkommen till ditt livs viktigaste spel.");
+Console.ReadLine();
+Console.WriteLine("Att svara rätt på dessa frågor är LIVSVIKTIGT");
+Console.ReadLine();
+Console.WriteLine("Den första frågan är väldigt simpel... Vad är ditt namn unga själ?");
+string name = Console.ReadLine();
+
+Console.WriteLine($"Välkommen till vad som kan vara ditt livs sista men definitivt viktisgaste spel, kära {name}");
+Console.ReadLine();
+Console.WriteLine("För att lämna detta spel med ditt liv så är det ytterst viktigt att du åtminstone får 50% rätt.");
+Console.ReadLine();
+
+Console.WriteLine($"Här kommer första frågan {name}. är du redo?");
+Console.ReadLine();
+Console.WriteLine("Fråga nr 1: I vilket land ligger paris ");
