92e7dfee0bd368ac790cf982dad87cbd8e81c515
Dean Nijenhuis <Dean.Nijenhuis@elev.ga.ntig.se>
2024-09-16 09:13:42 +0200

    1234567890qwertyuiopåaASDFGHJKLÖÄZXCVBNM

--- Quiz/Program.cs
+++ Quiz/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel;
 
-int points = 0; 
-
+int points = 0;
 Console.WriteLine("Hej och välkommen till ditt livs viktigaste spel.");
 Console.ReadLine();
 Console.WriteLine("Att svara rätt på dessa frågor är LIVSVIKTIGT");
@@ -17,15 +16,34 @@ Console.ReadLine();
 Console.WriteLine($"Här kommer första frågan {name}. är du redo?");
 Console.ReadLine();
 Console.WriteLine("Fråga nr 1: I vilket land ligger paris. A, Ryssland. B, Spanien. C, Belgien. D, Frankrike.");
-string answer = Console.ReadLine();
+string answer = Console.ReadLine(); 
 if (answer.ToLower() == "d")
 {
-    points ++;
+    points++;
     Console.WriteLine($"Grattis {name}, du fick din första fråga rätt.");
     Console.ReadLine();
-} else 
+}
+else
 {
+    Fel();
     Console.WriteLine("AJ, det gör ont för mig att se sådana patetiska misslyckande.");
     Console.ReadLine();
 }
 //------------------------------------------------------
+
+
+
+
+static void Fel()
+{
+    Console.WriteLine("""
+            
+       ______________________.____    ._.
+       \_   _____/\_   _____/|    |   | |
+        |    __)   |    __)_ |    |   | |
+        |     \    |        \|    |___ \|
+        \___  /   /_______  /|_______ \__
+            \/            \/         \/\/
+
+    """);
+}
